ab286e1608ac7296285d5f731298a6a1daa5aaeb
Steven Woodward <steveanthony999@gmail.com>
2024-01-21 20:50:51 -0800

    Updated json to include paths and links to projects.  Updated Info component to reflect dynamic changes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,36 +2,59 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { FaDisplay } from 'react-icons/fa6';
 import { FaCode } from 'react-icons/fa';
+import { TiDocumentText } from 'react-icons/ti';
+import { FaRss } from 'react-icons/fa';
 import './Info.css';
 import infoData from '../info.json';
 
 const Info = () => {
   const { projectInfo } = useSelector((state) => state.info);
-  const [title, setTitle] = useState();
-  const [description, setDescription] = useState();
+  const [project, setProject] = useState({});
+  // const [title, setTitle] = useState();
+  // const [description, setDescription] = useState();
+
+  // useEffect(() => {
+  //   setTitle(
+  //     infoData.projects.filter((data) => data.key === projectInfo)[0].title
+  //   );
+  //   setDescription(
+  //     infoData.projects.filter((data) => data.key === projectInfo)[0]
+  //       .description
+  //   );
+  // }, [projectInfo]);
 
   useEffect(() => {
-    setTitle(
-      infoData.projects.filter((data) => data.key === projectInfo)[0].title
+    const projectData = infoData.projects.find(
+      (data) => data.key === projectInfo
     );
-    setDescription(
-      infoData.projects.filter((data) => data.key === projectInfo)[0]
-        .description
-    );
+    setProject(projectData);
   }, [projectInfo]);
 
   return (
     <div className='Info'>
-      <h2>{title}</h2>
+      <h2>{project.title}</h2>
       <hr />
-      <p>{description}</p>
+      <p>{project.description}</p>
       <div className='link-container'>
-        <a href='#' target='_blank'>
-          <FaDisplay /> &nbsp;&nbsp;Site
-        </a>
-        <a href='#' target='_blank'>
-          <FaCode /> &nbsp;&nbsp;Code
-        </a>
+        {projectInfo === 'aboutme' ? (
+          <>
+            <a href={project.resume} target='_blank' rel='noopener noreferrer'>
+              <TiDocumentText /> &nbsp;&nbsp;Resume
+            </a>
+            <a href={project.blog} target='_blank' rel='noopener noreferrer'>
+              <FaRss /> &nbsp;&nbsp;Blog
+            </a>
+          </>
+        ) : (
+          <>
+            <a href={project.site} target='_blank' rel='noopener noreferrer'>
+              <FaDisplay /> &nbsp;&nbsp;Site
+            </a>
+            <a href={project.code} target='_blank' rel='noopener noreferrer'>
+              <FaCode /> &nbsp;&nbsp;Code
+            </a>
+          </>
+        )}
       </div>
     </div>
   );
