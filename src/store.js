ab297bbbd9c05a88e8148c950762089d64183d8c
Steven Woodward <steveanthony999@gmail.com>
2024-05-04 16:30:27 -0700

    removed everything

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import infoReducer from './slices/infoSlice';
-
-export const store = configureStore({
-  reducer: {
-    info: infoReducer,
-  },
-});
